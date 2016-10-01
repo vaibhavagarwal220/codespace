40935e99a8f787bd0e3d8bfad6ade63a75c7bb75
vaibhavagarwal220 <vaibhavagarwal220@gmaill.com>
2016-10-01 18:44:50 +0530

    changed welcome.php to show types of errors

--- welcome.php
+++ welcome.php
@@ -19,8 +19,9 @@ $time=time()+3.5*60*60;
  
 
   <style type="text/css">
-  .nt{margin-top:40px;}
   a{color:white;}
+  .btn{width:100%;}
+  #contain{width:70%;margin:auto;}
   </style>
 </head>
 <body>
@@ -29,14 +30,15 @@ $time=time()+3.5*60*60;
 include 'navbar.php'
  ?>
 
- <div class=page>
-
-<div class="btn btn-success"><i class=material-icons>done</i> AC (Accepted)</div>
-<div class="btn btn-danger"><i class=material-icons>highlight_off</i> WA (Wrong Answer)</div>
-<div class="btn btn-warning"><i class=material-icons>error_outline</i> RE (Runtime Error)</div>
-<div class="btn btn-info"><i class=material-icons>alarm</i> TLE (Time Limit Exceeded)</div>
-<div class="btn btn-primary"><i class=material-icons>warning</i> CE (Compilation Error)</div>
-      
+ <div id="contain">
+<h2>Judge Enviroment</h2>
+<div class="col-sm-6">
+<div class="btn btn-success"><i class=material-icons>done</i> AC (Accepted)</div><br><br>
+<div class="btn btn-danger"><i class=material-icons>highlight_off</i> WA (Wrong Answer)</div><br><br>
+<div class="btn btn-warning"><i class=material-icons>error_outline</i> RE (Runtime Error)</div><br><br>
+<div class="btn btn-info"><i class=material-icons>alarm</i> TLE (Time Limit Exceeded)</div><br><br>
+<div class="btn btn-primary"><i class=material-icons>warning</i> CE (Compilation Error)</div><br><br>
+      </div>
 
  </div>
 
